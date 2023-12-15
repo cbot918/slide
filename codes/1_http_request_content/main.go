fa17e17d786a936cd32cd349dc49eca6242baccb
cbot918 <cbot918@gmail.com>
2023-12-15 13:45:53 +0800

    update

--- codes/1_http_request_content/main.go
+++ codes/1_http_request_content/main.go
@@ -34,5 +34,15 @@ func main() {
 
 		fmt.Println(string(buf[:n]))
 
+		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nDate: Fri, 15 Dec 2023 03:58:55 GMT\r\nContent-Length: 27\r\nContent-Type: text/plain; charset=utf-8\r\nHello, you've requested: /\r\n\r\n")))
+
+		// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// 	fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+		// })
+
+		// // Start the HTTP server on port 8080
+		// fmt.Println(port)
+		// http.ListenAndServe(port, nil)
+
 	}
 }
---
+++ codes/3_gorilla_websocket/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func main() {
+
+	r := gin.Default()
+
+	r.GET("/ws", wsHandler)
+
+	r.Run(":8887")
+}
+
+func wsHandler(c *gin.Context) {
+	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		return
+	}
+	conn.WriteMessage(1, []byte("hihi"))
+	conn.WriteJSON(map[string]string{"a": "b"})
+}
